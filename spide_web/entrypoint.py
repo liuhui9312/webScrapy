acc8d02d701daf5bb9de7e9f5e5cecce5644dfb0
liuhui9312 <watching_blue_star@163.com>
2017-09-22 11:20:04 +0800

    scrapy test 1

---
+++ spide_web/entrypoint.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/13 11:09
+# @Author  : huiliu@annoroad.com
+# @Descript: 
+# @File    : entrypoint.py
+# @Software: PyCharm
+from scrapy.cmdline import execute
+execute(['scrapy', 'crawl', 'spide_web'])
